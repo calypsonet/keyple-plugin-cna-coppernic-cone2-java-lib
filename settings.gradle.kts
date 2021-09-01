6ad89f059f4161bdf85de47de2b4ac8b2fbb3197
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2021-09-01 16:01:31 +0200

    build: use snapshots

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
 include(":example-app")
 include(":coppernic-plugin")
 rootProject.name = "keyple-plugin-cna-coppernic-cone2-java-lib"
+
+// Fix resolution of dependencies with dynamic version in order to use SNAPSHOT first when available.
+// See explanation here : https://docs.gradle.org/6.8.3/userguide/single_versions.html
+enableFeaturePreview("VERSION_ORDERING_V2")
